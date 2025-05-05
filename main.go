23fe5b0013709d2695b9d543fc0693ac536a97e1
Anand <anandkkumar003@gmail.com>
2025-05-05 22:00:25 +0530

    prices

--- main.go
+++ main.go
@@ -1,20 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"example.com/price-calculator/prices"
+)
 
 func main() {
-	prices := []float64{3, 4, 5}
 	taxRates := []float64{0.1, 0.2, 0.3}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		taxIncludedPrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-		}
-		result[taxRate] = taxIncludedPrices
+		priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+		priceJob.Calculate()
 	}
 
-	fmt.Println(result)
 }
